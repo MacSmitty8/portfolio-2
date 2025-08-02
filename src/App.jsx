57d63742d4b81bc6e1d5e68130069671128524b0
MacreenSmith <macreensmith@gmail.com>
2025-08-02 13:51:08 -0400

    Created the "About" component/section and added elements to it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Header from './Components/Header/Header'
+import About from './Components/About/About'
 
 const App = () => {
   return (
     <div>
-    <Navbar />  
-    <Header />
+      <Navbar />
+      <Header />
+      <About />
     </div>
   )
 }
