f9e285c6e730441b7eb433ca0b07027cb4df5e8d
MacreenSmith <macreensmith@gmail.com>
2025-08-06 20:21:09 -0400

    Applied CSS changes to the about section and component.

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,17 +8,20 @@ const About = () => {
     <div className='about'>
       <div className='about-title'>
         <h1>About Me</h1>
-        <div className="about-sections">
-          <div className="about-left">
-            <img src={profile_photo2} alt='' />
-          </div>
-          <div className="about-right">
-            <div className="about-para">
-              <p>"I'm a Computer Science graduate with a strong passion for web development and a commitment to continuous learning. 
-              I enjoy building responsive, user-friendly applications using technologies like JavaScript, React, and MongoDB. 
-              I’ve also gained hands-on experience developing interactive projects, including games built with Unity. Detail-oriented and focused under pressure, 
-              I’m eager to contribute to a collaborative team and grow as a developer while creating meaningful digital experiences."</p>
-            </div>
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_photo2} alt='' />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>I'm a Computer Science graduate with a strong passion for web development and a commitment to continuous learning.
+              I enjoy building responsive, user-friendly applications using technologies like JavaScript, React, and MongoDB.
+              I’ve also had hands-on experience developing interactive projects, including games built with Unity. Detail-oriented and focused under pressure,
+              I’m eager to contribute to a collaborative team and grow as a developer while creating meaningful digital experiences.</p>
+
+            <p>In addition to my coding skills, I'm currently learning Cybersecurity, AWS, and AI related functions such as LLM's.
+              I also have experience with package handling and freight. </p>
           </div>
         </div>
       </div>
